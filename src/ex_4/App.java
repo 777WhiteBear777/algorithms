462f4fa0de0ccf131d23602a506df9fecb6137db
WB_777 <antariuzz@gmail.com>
2023-01-08 20:32:15 +0200

    Fourth exercise done

---
+++ src/ex_4/App.java
@@ -0,0 +1,38 @@
+package ex_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+
+            System.out.println("Enter the equation: ");
+            String line = reader.readLine();
+
+            int openSymbol = line.indexOf('(');
+            int closeSymbol = line.indexOf(')');
+
+            if (openSymbol >= 0 && closeSymbol >= 0) {
+                if (openSymbol < closeSymbol) {
+                    System.out.println("All good!");
+                } else if (openSymbol > closeSymbol) {
+                    System.out.println("Error ");
+                }
+            }
+
+            if (openSymbol < 0 && closeSymbol >= 0) {
+                System.out.println("Open parenthesis missing!");
+            } else if (openSymbol >= 0 && closeSymbol < 0) {
+                System.out.println("Close parenthesis missing!");
+            } else {
+                System.out.println("Parenthesis missing!");
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
